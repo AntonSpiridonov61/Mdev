fa243d134a4f803b1bb1fc3eddcdffc2953dbfc1
AntonSpiridonov61 <spiridonov61a@gmail.com>
2022-02-22 22:06:58 +0800

    add

--- Weather/app/src/main/java/com/example/wearher/MainActivity.kt
+++ Weather/app/src/main/java/com/example/wearher/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.wearher
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,43 +21,80 @@ import com.google.gson.Gson
 import org.json.JSONObject
 import org.json.JSONTokener
 import java.io.InputStreamReader
+import androidx.databinding.DataBindingUtil
+import android.net.ConnectivityManager
+import android.widget.Toast
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+    lateinit var connectivityManager: ConnectivityManager
+    lateinit var dataWeather: DataWeather
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         binding.btnGetWeather.setOnClickListener {
             GlobalScope.launch (Dispatchers.IO) {
                 loadWeather()
             }
         }
-
-        /*
-        TODO: реализовать отображение погоды в текстовых полях и картинках
-        картинками отобразить облачность и направление ветра
-        использовать data binding, библиотеки уже подключены
-         */
     }
+
+    suspend fun setImage(url: String) {
+//        Glide.with(this)
+//            .load(url)
+//            .into(binding.ivDescriptions)
+        var imageWeather : Bitmap? = null
+        val imgStream = URL(url).openStream() as InputStream
+        imageWeather = BitmapFactory.decodeStream(imgStream)
+
+        if (imageWeather !== null) {
+            GlobalScope.launch (Dispatchers.Main) {
+                binding.ivDescriptions.setImageBitmap(imageWeather)
+            }
+        }
+    }
+
     suspend fun loadWeather() {
-        val API_KEY = resources.getString(R.string.keyAPI)
-        // TODO: в строку подставлять API_KEY и город (выбирается из списка или вводится в поле)
-        val weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Irkutsk&appid=$API_KEY&units=metric";
-        val stream = URL(weatherURL).getContent() as InputStream
-        // JSON отдаётся одной строкой,
-        val data = Scanner(stream).nextLine()
-        // TODO: предусмотреть обработку ошибок (нет сети, пустой ответ)
-        Log.d("mytag", data)
-//        val jsonObject = JSONTokener(data).nextValue() as JSONObject
-//
-//        val id = jsonObject.getString("coord")
-//        Log.i("ID: ", id)
+        Log.d("WeatherConnection", connectivityManager.isActiveNetworkMetered.toString())
+        if (true) {
+            val gson = Gson()
 
-        var gson = Gson()
-        var testModel = gson.fromJson(InputStreamReader(stream), DataWeather::class.java)
+            val API_KEY = resources.getString(R.string.keyAPI)
+            val city = binding.cityInp.text.toString()
+            val weatherURL =
+                "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$API_KEY&units=metric";
+            val stream = URL(weatherURL).getContent() as InputStream
+
+            // JSON отдаётся одной строкой,
+            val data = Scanner(stream).nextLine()
+            Log.d("data", data)
+            val dataWeather = gson.fromJson(data, DataWeather::class.java)
+            binding.dataWeather = dataWeather
+
+
+            val imageURL = "http://openweathermap.org/img/wn/${dataWeather.weather[0].icon}@2x.png"
+            setImage(imageURL)
+
+            val deg = dataWeather.wind.deg
+            GlobalScope.launch (Dispatchers.Main) {
+                if (135 > deg && deg >= 45) {
+                    binding.ivWind.setImageResource(R.drawable.east)
+                } else if (225 > deg && deg >= 135) {
+                    binding.ivWind.setImageResource(R.drawable.south)
+                } else if (315 > deg && deg >= 225) {
+                    binding.ivWind.setImageResource(R.drawable.west)
+                } else {
+                    binding.ivWind.setImageResource(R.drawable.north)
+                }
+            }
+        } else {
+            var toast = Toast.makeText(this, "No Internet", Toast.LENGTH_SHORT).show()
+        }
     }
-
 }
